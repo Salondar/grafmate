25a4ec76c5be01ea789a66ded684994fdd5eb769
Salondar <mrvllscbl41@gmail.com>
2025-02-26 18:03:35 +0200

    Add basic file functions

---
+++ src/canvas.js
@@ -0,0 +1,14 @@
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+function drawLines() {
+
+}
+
+function plotFunction() {
+
+}
+
+function runMonotonicityAnimation() {
+    
+}
