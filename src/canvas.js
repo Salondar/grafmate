7a9d2627093801f636cab0b3daea3dccb6764f31
Salondar <mrvllscbl41@gmail.com>
2025-03-03 18:00:34 +0200

    Remove drawLines Function

--- src/canvas.js
+++ src/canvas.js
@@ -1,51 +1,68 @@
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
-
-const SCALING_FACTOR = 10;
+const SCALING_FACTOR = 15;
 const NUMBER_SCALE = SCALING_FACTOR * 5;
-canvas.width = canvas.clientWidth;
-canvas.height = canvas.clientHeight;
 
-function drawLines(lineType) {
-    ctx.save();
-    ctx.translate(canvas.width, canvas.height); // Go to the center of the canvas
+function drawVerticalLines() {
+    let x = 0;
+    while (x < canvas.width) {
 
-    let size, i = 0;
-
-    if (lineType === "h") {
-        size = canvas.width;
-    }
-    else if (lineType === "v") {
-        size = canvas.height;
-    }
-
-    while (i < size / 2) {
-        ctx.beginPath();
-        if (i === 0) {
-            ctx.lineWidth = 1;
-        }
-        else if (i % NUMBER_SCALE === 0) {
+        if (x === 0) {
             ctx.lineWidth = 0.3;
-        }   ctx.beginPath();
-        ctx.moveTo(i, -canvas.height);
-        ctx.lineTo(i, canvas.height);
+        }
+        else if (x % NUMBER_SCALE === 0) {
+            ctx.lineWidth = 0.2;
+        }
+        ctx.beginPath();
+        ctx.moveTo(x, -canvas.height);
+        ctx.lineTo(x, canvas.height);
         ctx.stroke();
 
         ctx.beginPath();
-        ctx.moveTo(-i, -canvas.height);
-        ctx.lineTo(-i, canvas.height);
+        ctx.moveTo(-x, -canvas.height);
+        ctx.lineTo(-x, canvas.height);
         ctx.stroke();
 
-        i += NUMBER_SCALE;
+        x += SCALING_FACTOR;
+        ctx.lineWidth = 0.1;
+    }
+}
+
+function drawHorizontalLines() {
+    let y = 0;
+
+    while (y < canvas.height) {
+        if (y === 0) {
+            ctx.lineWidth = 0.3;
+        }
+        if (y % NUMBER_SCALE === 0) {
+            ctx.lineWidth = 0.2;
+        }
+        ctx.beginPath();
+        ctx.moveTo(-canvas.height, y);
+        ctx.lineTo(canvas.height, y);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(-canvas.height, -y);
+        ctx.lineTo(canvas.height, -y);
+        ctx.stroke();
+
+        y += SCALING_FACTOR;
         ctx.lineWidth = 0.1
     }
 }
 
 function drawGrid() {
-    drawLines("v"); // Draw horizontal lines
-    //drawLines(); // Draw vertical lines
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    ctx.save();
+    ctx.translate(canvas.width / 2, canvas.height / 2); // Go to the center of the canvas
+    drawVerticalLines();
+    drawHorizontalLines();
 }
+
 function plotFunction() {
 
 }
