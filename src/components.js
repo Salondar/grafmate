d967b895f22e0f49174721f37c54ffea68f77b58
Salondar <mrvllscbl41@gmail.com>
2025-03-03 18:57:17 +0200

    Add attribute to element in createInputField function

--- src/components.js
+++ src/components.js
@@ -2,6 +2,7 @@ const inputDiv = document.querySelector(".input");
 
 function createInputField() {
     const input = document.createElement("input");
+    input.setAttribute("id", "expression");
     input.type = "text";
     input.placeholder = "Expressão...";
     inputDiv.appendChild(input);
