c2f88650ed5f2058e75b9666fcfc5bf96b6fa4f3
Salondar <mrvllscbl41@gmail.com>
2025-03-06 10:01:35 +0200

    Add drawCurrentMonotonicityPic function

--- src/canvas.js
+++ src/canvas.js
@@ -119,6 +119,9 @@ function runMonotonicityAnimation(start, end) {
     
 }
 
+function drawCurrentMonotonicityFrame(xval) {
+    
+}
 function prepareForRedraw() {
     ctx.restore();
     ctx.clearRect(0, 0, canvas.width, canvas.height)
