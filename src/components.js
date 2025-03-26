2e5c18104191bcebf6251e7e455e633edc4d8d58
Salondar <mrvllscbl41@gmail.com>
2025-03-26 16:39:19 +0200

    Add declarations

--- src/components.js
+++ src/components.js
@@ -1,15 +1,17 @@
 const inputDiv = document.querySelector(".input");
+const sliderContainer = document.querySelector("#slider-container");
+const btnContainer = document.querySelector("#button-container");
+const expressionField = document.querySelector(".expression-field");
 
 function createInputField() {
     const input = document.createElement("input");
     input.setAttribute("id", "expression");
     input.type = "text";
     input.placeholder = "Expressão...";
-    inputDiv.appendChild(input);
+    expressionField.appendChild(input);
 }
 
 function createSlider() {
-    const sliderContainer = document.querySelector("#slider-container");
     const slider = document.createElement("div");
     slider.setAttribute("id", "slider");
 
@@ -29,11 +31,9 @@ function createSlider() {
 }
 
 function createYesNoButtons () {
-    const buttonContainer = document.createElement("div");
     const questionPara = document.createElement("p");
     questionPara.textContent = "Adicionar um deslizador?"
-    buttonContainer.setAttribute("id", "button-container");
-    buttonContainer.appendChild(questionPara);
+    btnContainer.appendChild(questionPara);
 
     const yesButton = document.createElement("button");
     yesButton.setAttribute("id", "yes-button");
@@ -42,9 +42,8 @@ function createYesNoButtons () {
     const noButton = document.createElement("button");
     noButton.setAttribute("id", "no-button");
     noButton.textContent = "não";
-
-    inputDiv.appendChild(buttonContainer);
-    buttonContainer.appendChild(yesButton);
-    buttonContainer.appendChild(noButton);
+    
+    btnContainer.appendChild(yesButton);
+    btnContainer.appendChild(noButton);
 }
 export {createInputField, createSlider, createYesNoButtons};
